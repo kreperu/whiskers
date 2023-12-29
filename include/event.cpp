16de020556e1f2c4e83299c1dd4132a878cd66a1
kreperu <thekreperu@gmail.com>
2023-12-30 00:39:54 +0100

    define addToHandler

--- include/event.cpp
+++ include/event.cpp
@@ -5,5 +5,14 @@ bool EventHandler::handleEvents(std::vector<Tile*>* tiles) {
 }
 
 int EventHandler::addToHandler(Tile* tile) {
-	return 0;
+	this->tiles.push_back(tile);
+	int id = this-> tilesId.size();
+	this->tilesId[id] = tile;
+	this->idPropertiesVal[id] = tile->prop;
+	auto keyView = std::views::keys(tile->prop);
+	std::vector<char*> keys{keyView.begin(), keyView.end()};
+	this->idPropertiesHas[id] = keys;
+	std::vector<int> xyz = {tile->x, tile->y, tile->z};
+	this->tilesXYZ[xyz] = tile;
+	return id;
 }
--- include/event.hpp
+++ include/event.hpp
@@ -5,22 +5,32 @@
 #include <vector>
 #include <map>
 #include <any>
+#include <ranges>
 
 class EventHandler {
 	public:
+		// Vector of all Tile* in EventHandler
 		std::vector<Tile*> tiles;
+		// Map of Tile id -> Tile with id
 		std::map<int, Tile*> tilesId;
+		// Map of Tile id -> map<propName -> propVal>
 		std::map<int, std::map<char*, std::any>> idPropertiesVal;
+		// Map of Tile id -> propName
 		std::map<int, std::vector<char*>> idPropertiesHas;
+		// Map of vector<x, y, z> -> Tile on x, y, z
 		std::map<std::vector<int>, Tile*> tilesXYZ;
 		
+		// Handle all events
 		bool handleEvents(std::vector<Tile*>* tiles);
+		// Add tile to handler, return its id
 		int addToHandler(Tile* tile);
+		// Return Tile with the id id
 		Tile* getTileById(int id);
+		// Return Tile on position x, y, z
 		Tile* getTileByXYZ(int x, int y, int z);
-		// Return vectow of tiwes* with pwopewty (propName) == vawue (propVal)
+		// Return vectow of tiwes (Tile*) with pwopewty (propName) == vawue (propVal)
 		std::vector<Tile*> getTilesByPropVal(char* propName, std::any propVal);
-		// Return vectow of tiwes* containing pwopewty (propName)
+		// Return vectow of tiwes (Tile*) containing pwopewty (propName)
 		std::vector<Tile*> getTilesByPropHas(char* propName);
 };
 
